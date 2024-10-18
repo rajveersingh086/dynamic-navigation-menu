659f84f0a9f0bedf4e02a8185113e690de21ffe7
Rajveer Singh <138320694+rajveersingh086@users.noreply.github.com>
2024-10-18 22:16:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navbar = document.getElementById('navbar');
+    const links = navbar.querySelectorAll('a');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+
+    window.addEventListener('scroll', function () {
+        let fromTop = window.scrollY + 60;
+
+        links.forEach(link => {
+            let section = document.querySelector(link.hash);
+
+            if (
+                section.offsetTop <= fromTop &&
+                section.offsetTop + section.offsetHeight > fromTop
+            ) {
+                link.classList.add('active');
+            } else {
+                link.classList.remove('active');
+            }
+        });
+    });
+});
